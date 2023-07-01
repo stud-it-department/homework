e74a0ebc7cbf8cb1bf1008454473b4a3fa7f2194
shaa22u355 <89980778+Ross-the-Moose@users.noreply.github.com>
2023-07-02 01:10:30 +0300

    first commit

--- main.go
+++ main.go
@@ -1,5 +1,121 @@
 package main
 
-func main() {
+/*
+Если цифры в названии - убрать числа
+Если только цифры     - сделать пометку
+Если нет цифр         - upper
+*/
 
+import (
+	// pkg "./pkg"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func caseNumbers(fileName string) int {
+	/*
+		-1 if no numbers
+		 0 if some numbers
+		 1 if all numbers
+	*/
+	strLen := 0
+	digitCnt := 0
+	for _, ch := range fileName {
+		if ch == '.' {
+			break
+		}
+
+		if '0' <= ch && ch <= '9' {
+			digitCnt++
+		}
+		strLen++
+	}
+	if digitCnt == 0 {
+		return -1
+	}
+	if digitCnt == strLen {
+		return 1
+	}
+	return 0
+}
+
+func removeNumbers(str string) string {
+	strNoNums := ""
+	for _, ch := range str {
+		if !('0' <= ch && ch <= '9') {
+			strNoNums += string(ch)
+		}
+	}
+	return strNoNums
+}
+
+func findDot(str string) int {
+	for ind, ch := range str {
+		if ch == '.' {
+			return ind
+		}
+	}
+	return -1
+}
+
+func fileNameUpper(str string) string {
+	strUpper := ""
+	dotPos := findDot(str)
+	for i, ch := range str {
+		if i < dotPos {
+			strUpper += strings.ToUpper(string(ch))
+		} else {
+			strUpper += string(ch)
+		}
+	}
+	return strUpper
+}
+
+func main() {
+	var dirName string
+	var infoFile string
+
+	// input output file
+	fmt.Printf("input info-file title: ")
+	fmt.Scanf("%s", infoFile)
+
+	// input directory name
+	fmt.Printf("input dir name: ")
+	fmt.Scanf("%s", &dirName)
+
+	// open dir and read files in it
+	dir, err := os.Open(dirName)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		files, err := dir.ReadDir(0)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			// open output file for writing
+			outFile, err := os.OpenFile(infoFile, os.O_WRONLY, 0666)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				for _, file := range files {
+					if !file.IsDir() {
+						fileName := file.Name()
+						switch caseNumbers(fileName) {
+						case -1: // upper fileName
+							outFile.WriteString(fileNameUpper(fileName))
+							fmt.Println(fileNameUpper(fileName))
+						case 0: // remove numbers from fileName
+							outFile.WriteString(removeNumbers(fileName))
+							fmt.Println(removeNumbers(fileName))
+						case 1: // make a note abt it
+							message := "this file " + fileName + " is special, don't touch it"
+							outFile.WriteString(message)
+						}
+					}
+				}
+				outFile.Close()
+			}
+		}
+	}
 }
